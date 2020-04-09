ec907a64671e56422447d9c78675c22f65ca3359
George Khoury <geokhoury@users.noreply.github.com>
2020-04-09 12:05:24 +0300

    Fixed issue with timestamp formatter

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@
 echo "Running the build script..."
 
 echo "I am ${HOSTNAME}."
-echo "The time is $(date %s)."
+echo "The time is $(date '%s')."
 echo "The logged in user is: ${USER}."
 echo "Current working directory: ${PWD}."
