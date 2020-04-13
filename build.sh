4672b83eebe06bee6182039df10c243f691b7b61
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-13 19:59:51 +0300

    nthing

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ echo "Building a webpage..."
 
 # Declare and initialize values
 SERVER_NAME=$HOSTNAME
-#echo ${SERVER_NAME} 1 > web/index.html
-sudo chmod -R u+rwx /web/index.html
+echo ${SERVER_NAME} 1 > web/index.html
+#sudo chmod -R u+rwx /web/index.html
 #echo ${PWD} 1 > web/index.html
- ./web/index.html    ${SERVER_NAME}  
+ #./web/index.html    ${SERVER_NAME}  
 
 # Replace values in index.html
 ## TODO: Replace {SEVER_NAME} with $SEVER_NAME
