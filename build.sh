8135c821c58f8e2568268c94e0c8ee1d75046103
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-13 19:28:57 +0300

    add value

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ echo "Building a webpage..."
 
 # Declare and initialize values
 SERVER_NAME=$HOSTNAME
-#echo ${SERVER_NAME} 1 > web/index.html
- ./web/index.html    ${SERVER_NAME}  
+echo ${SERVER_NAME} 1> web/index.html
+echo ${PWD} 1 > web/index.html
+# ./web/index.html    ${SERVER_NAME}  
 
 # Replace values in index.html
 ## TODO: Replace {SEVER_NAME} with $SEVER_NAME
