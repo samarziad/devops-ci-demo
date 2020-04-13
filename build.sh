e66d0d8ca435d92032d151871f581e9800630f65
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-13 18:43:51 +0300

    ${SERVER_NAME}  lcatin

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ echo "Building a webpage..."
 # Declare and initialize values
 SERVER_NAME=$HOSTNAME
 #echo ${SERVER_NAME} 1 > web/index.html
-   ${SERVER_NAME}   ./web/index.html 
+ ./web/index.html    ${SERVER_NAME}  
 
 # Replace values in index.html
 ## TODO: Replace {SEVER_NAME} with $SEVER_NAME
