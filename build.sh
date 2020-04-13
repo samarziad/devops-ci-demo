e3b6a6923a2448fdeefa5007fe16276fef12c6fb
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-13 06:48:23 +0300

    server name

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ echo "Building a webpage..."
 
 # Declare and initialize values
 SERVER_NAME=$HOSTNAME
+${SERVER_NAME} >> web/
 
 # Replace values in index.html
 ## TODO: Replace {SEVER_NAME} with $SEVER_NAME
