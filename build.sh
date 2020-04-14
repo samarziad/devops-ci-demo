5f94354402a6720cb265e25832ace591d0d85149
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-14 20:40:59 +0300

    ex

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ echo "Building a webpage..."
 
 # Declare and initialize values
  export  SERVER_NAME=${HOSTNAME}
+ sed -i "s/{/\${/g" web/index.html
+envsubst '$TIMESTAMP $GIT_REPO $GIT_BRANCH $AZURE_VARIABLE $USER $PWD $SERVER_NAME $TIMESTAMP' > web/newIndex.html < web/index.html 
 #echo ${SERVER_NAME} 1 > web/index.html
 
 #echo "vso[task.setvariable variable=${HOSTNAME};]SERVER_NAME"
