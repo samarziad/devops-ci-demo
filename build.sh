8c43f4aac667d61b3ecf14d6dabf2b29d0c2aaeb
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-16 23:19:54 +0300

    ${HOSTNAME}

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 echo "Building a webpage..."
 
 # Declare and initialize values
- export SERVER_NAME=$(Agent.MachineName)
+SERVER_NAME=$HOSTNAME
  $sed  's/{SERVER_NAME}/SERVER_NAME/g' web/index.html
 #envsubst '$SERVER_NAME' > web/newIndex.html  
 #echo ${SERVER_NAME} 1 > web/index.html
