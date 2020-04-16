3a15086d100a96c171f13ec0d4a32f33561bc727
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-16 22:12:22 +0300

    switch

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@
 echo "Building a webpage..."
 
 # Declare and initialize values
- export  SERVER_NAME=${HOSTNAME}
+ SERVER_NAME=${HOSTNAME}
  
- $sed 's/{SERVER_NAME}/SERVER_NAME/g' web/index.html
+ $sed 's/SERVER_NAME/{SERVER_NAME}/g' web/index.html
 #envsubst '$SERVER_NAME' > web/newIndex.html  
 #echo ${SERVER_NAME} 1 > web/index.html
 
