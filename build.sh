d43c6a8701e15d1586737ee19c9cd214c6e3122f
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-17 17:30:38 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ USER=${USER}
 
 
 DATE=$(date)
-GIT_REPO=$(get remote.origin.url)
+GIT_REPO=$(git rev-parse --git-dir)
 Pwd=$(pwd)
 #echo "##vso[task.setvariable variable=BUILD_DATE;]$DATE"
 #branch_name=$(Build.SourceBranch)
