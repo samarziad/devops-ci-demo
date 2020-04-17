62171fa064c4d0fa4761247f615f25aa4134a779
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-17 20:47:44 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,9 +5,10 @@
 
 
 # Define path for release ---> index.html (release)
-string path = index.html 
+  string path = "index.html" 
  if(!File.Exists(path))
 exit 3
  else 
    echo "build was successfull. Test passed."
 exit 0
+fi
