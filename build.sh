e6859cc0f3fb74a1cf9816278ada8762d6ff845f
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-17 18:15:30 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ USER=${USER}
 
 
 DATE=$(date)
-GIT_REPO=$(git remote -v | head -n1  | sed 's/.*\///' | sed 's/\.git//')
+GIT_REPO=$(git remote -v | sed 's/.*\///' | sed 's/\.git//')
 
 Pwd=$(pwd)
 #echo "##vso[task.setvariable variable=BUILD_DATE;]$DATE"
