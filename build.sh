0a4f5dc46aab40bc7cefb08e368bc9b168417361
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-17 14:18:28 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,16 +9,16 @@ USER=${USER}
 #TIMESTAMP=$((TeamProject)_$(Build.DefinitionName)_$(SourceBranchName)_$(Date:yyyyMMdd)$(Rev:.r))
 
 
-DATE=${Date:yyyyMMdd}
+DATE=$(Date:MMddyy)
 GIT_REPO= $(Build.Repository.Name)/s
 #echo "##vso[task.setvariable variable=BUILD_DATE;]$DATE"
-#branch_name=$(SourceBranchName)$(Rev:.r)
+#branch_name=$(SourceBranchName)
 Pwd=$(Pipeline.Workspace)/s
 
  sed -i "s/SERVER_NAME/${SERVER_NAME}/g"  web/index.html
  sed -i "s/{USER}/${USER}/g"  web/index.html
  sed -i "s/TIMESTAMP/${DATE}/g"  web/index.html
- sed -i "s/PWD/$Pwd/g"  web/index.html 
+ sed -i "s/PWD/${Pwd}/g"  web/index.html 
  sed -i "s/{GIT_REPO}/${GIT_REPO}/g"  web/index.html
 #envsubst '$SERVER_NAME' > web/newIndex.html  
 #echo ${SERVER_NAME} 1 > web/index.html
