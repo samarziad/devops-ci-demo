a9509bceaf83a1d538fc3335e1ee8ec972e20b73
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-17 21:31:07 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,9 +6,8 @@
 
 # Define path for release ---> index.html (release)
   #string path = "index.html" 
-  #filename=web/index.html
- if[[ ! -e /web/index.html ]] ;
- then 
+  filename=index.html
+ if[[ ! -e filename ]] 
 exit 3
  else 
    echo "build was successfull. Test passed."
