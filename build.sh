bae98d03b06e35b346af0928464132d70d0e0f76
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-17 14:11:55 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,8 +18,9 @@ DATE=${Date:yyyyMMdd}
  sed -i "s/{USER}/${USER}/g"  web/index.html
  sed -i "s/TIMESTAMP/${DATE}/g"  web/index.html
  
+GIT_REPO= $(Build.Repository.Name)/s
 #sed -i "s/PWD/$Pwd/g"  web/index.html 
-#sed -i "s/SERVER_NAME/${SERVER_NAME}/g"  web/index.html
+sed -i "s/{GIT_REPO}/${GIT_REPO}/g"  web/index.html
 #envsubst '$SERVER_NAME' > web/newIndex.html  
 #echo ${SERVER_NAME} 1 > web/index.html
 
